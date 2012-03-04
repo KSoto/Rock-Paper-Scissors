bc7af768a50681b1b06591dc92389bff4bd71085
Katie Soto <KReneSoto@gmail.com>
2012-03-04 12:39:29 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RockPaperScissors::Application.config.secret_token = '896969d9fe3b9e35458a54378ccaee9b1724c31b40c381cb0660185adfb58df36eaa93e2517802680b0dfb2904d627636ffb9e3b99071f812ce92e2ce874a0a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RockPaperScissors::Application.config.session_store :cookie_store, :key => '_rock_paper_scissors_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RockPaperScissors::Application.config.session_store :active_record_store
