cd4fa0875614c8978e4b5ee5904707ecb09961f4
Katie Soto <KReneSoto@gmail.com>
2012-03-04 15:06:04 -0800

    Routes finished, working on throw controller and my_throw view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
-  @throws = @defeat.keys
-  @computer_throw = @throws.sample
-
 end
--- app/controllers/throw_controller.rb
+++ app/controllers/throw_controller.rb
@@ -1,4 +1,15 @@
 class ThrowController < ApplicationController
-  def my_throw
-  end
+before_filter do
+  @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
+  @throws = @defeat.keys
+  @computer_throw = @throws.sample
+end
+
+def my_throw
+    @player_throw = params[:type].to_sym
+    if !@throws.include?(@player_throw)
+        #halt 403, "You must throw one of the following: #{@throws}"
+    end
+end
+
 end
