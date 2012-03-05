a352aa25c65f3ea2ea5fb035a6ffcce445456d91
Katie Soto <KReneSoto@gmail.com>
2012-03-04 17:11:55 -0800

    Implemented the 'clear' section

--- app/controllers/clear_controller.rb
+++ app/controllers/clear_controller.rb
@@ -1,9 +1,6 @@
 class ClearController < ApplicationController
   def index
-	#content_type = :txt
-    	#session[:counter]=0
-    	#session[:history] = []
-    	#"Session Cleared"
+    	session[:counter]=0
+    	session[:history].clear
   end
-
 end
