19b91bff3eabe94c4741e1f5d854a3087c925897
Katie Soto <KReneSoto@gmail.com>
2012-03-04 16:03:37 -0800

    Changed root index to show instructions instead

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ RockPaperScissors::Application.routes.draw do
   #this matches parameters after 'throw' and puts it in 'type':
   #parameters: {"type" => "paper" (or whatever you enter}
   #after the =>: will go to the 'throw' controller and whatever is after the  #'#' is the action it goes to within the throw controller.
+  # so it's: ____ 'url_match' => 'controller#controller_action'
 
   match 'stats' => 'stats#index'
 
   match 'clear' => 'clear#index'
 
   match 'throw' => 'throw#instructions'
+  #localhost:3000/throw will go to instructions
+  #localhost:3000/throw/ will go to instructions
 
-  match '' => 'throw#instructions'
+  root :to => 'throw#instructions'
+  #root (localhost:3000/) will go to instructions
 
 end
