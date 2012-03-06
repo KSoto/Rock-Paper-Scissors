20d9b37c24f3f5b02ce7c358fa4995aa92fc30af
Katie Soto <KReneSoto@gmail.com>
2012-03-05 18:13:34 -0800

    Trying RSpec Tests

--- spec/controllers/throw_controller_spec.rb
+++ spec/controllers/throw_controller_spec.rb
@@ -1,13 +1,31 @@
 require 'spec_helper'
 
 describe ThrowController do
-  @my_throw = "rock"
-  @comp_throw = "paper"
- 
   describe "GET random page (rock, paper, or scissors)" do
     it "should be successful" do
+		@defeat = {rock: :scissors, paper: :rock, scissors: :paper}
+		@throws = @defeat.keys
+		@my_throw = @throws.sample
+  		@comp_throw = @throws.sample
       match 'throw/@my_throw' => 'throw#my_throw'
       response.should be_success
     end
+  end
+
+  describe "Winners and Losers" do
+	it "should win, lose or tie" do
+		@defeat = {rock: :scissors, paper: :rock, scissors: :paper}
+		@throws = @defeat.keys
+		@my_throw = @throws.sample
+  		@comp_throw = @throws.sample
+		match 'throw/@my_throw' => 'throw#my_throw'
+		if @my_throw==@comp_throw
+			@result.should == "You tied."
+    		elsif @comp_throw == @defeat[@my_throw] 
+      			@result.should == "You Won!"
+    		else
+       		@result.should == "You Lost!"
+    		end
+	end
   end
 end
