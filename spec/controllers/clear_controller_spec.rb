a8d825a5515e59271832991e9153679b77b56ff1
Katie Soto <KReneSoto@gmail.com>
2012-03-06 22:50:19 -0800

    Clear RSpec Tests working

--- spec/controllers/clear_controller_spec.rb
+++ spec/controllers/clear_controller_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 describe ClearController do
+ #render_views is NOT automatic. Need to put it here!
+  render_views
 
+  #green
   describe "GET clear" do
     it "should be successful" do
       get 'index'
       response.should be_success
     end
+  end
+
+  #green
+  describe "GET clear page" do
+  it "should show clear history message" do
+		get :index
+		response.should have_selector("h1", :content => "Clear history")
+	end
   end
 
 end
