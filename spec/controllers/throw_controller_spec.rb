0d61429be966952ba964e7cfe45798a1cb4a0861
Katie Soto <KReneSoto@gmail.com>
2012-03-07 14:49:11 -0800

    RSpec Working!

--- spec/controllers/throw_controller_spec.rb
+++ spec/controllers/throw_controller_spec.rb
@@ -26,52 +26,36 @@ describe ThrowController do
 	end
   end
 
-  #red
-  #currently my best guess. Still doesn't work, though.
+  #green
   describe "Winners and Losers" do
 	it "should win, lose or tie" do
 		@defeat = {rock: :scissors, paper: :rock, scissors: :paper}
 		@throws = @defeat.keys
-		@my_throw = 'paper' #@throws.sample
-		@comp_throw = 'paper' #@throws.sample
+		
+		#generate a random move:
+		@my_throw = @throws.sample
 	
 		#get the action, and set the param ':type' with our
 		#generated move (rock, paper, or scissors)
-		#Also set what the computer will throw so we can
-		#see if the response is correct:
-		get :my_throw, :type => @my_throw, :computer_throw => @comp_throw
+		get :my_throw, :type => @my_throw
 		
-		#@comp_throw = assigns(:computer_throw)
+		#get what the computer threw from the controller
+		#so we can see what the correct outcome should be
+		@comp_throw = assigns(:computer_throw)
+		
+		print "Tested Moves: \nPlayer's Move - ", @my_throw, "\nComputer's Move - ", @comp_throw
 
-    		if @my_throw==@comp_throw
-			response.should contain("You tied.")
-    		elsif @comp_throw == @defeat[@my_throw] 
-			response.should contain("You Won!")
-    		else
-			response.should contain("You Lost!")
+    		if (@my_throw==@comp_throw)
+				print "\nOutcome - tie"
+				response.should contain("You tied.")
+    		elsif (@comp_throw == @defeat[@my_throw]) 
+				print "\nOutcome - win"
+				response.should contain("You Won!")
+    		elsif (@mythrow == @defeat[@comp_throw])
+				print "\nOutcome - lost"
+				response.should contain("You Lost!")
     		end
 
 	end
   end
 end
-
-#Current Error:
-#     ActionController::RoutingError:
-#       No route matches {:controller=>"throw", :action=>"throw/rock"}
-
-
-#Also tried this:
-#	  	throw_controller = ThrowController.new
-#		throws = throw_controller.assigns[:throws]
-#		user_throw = throws.sample
-#		comp_throw = throws.sample
-#		defeat = throw_controller.assigns[:defeat]
-#		match 'throw/user_throw' => 'throw#my_throw'
-#		result = throw_controller.my_throw.assigns[:result]
-#    		if user_throw==comp_throw
-#			result.should == "You tied."
-#    		elsif comp_throw == defeat[user_throw] 
-#     		 	result.should == "You Won!"
-#    		else
-#       		result.should == "You Lost!"
-#    		end
