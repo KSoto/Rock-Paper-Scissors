606ef0525919f049132d2cb9d2233830c7725fbf
Katie Soto <KReneSoto@gmail.com>
2012-03-06 22:49:53 -0800

    Stats RSpec Tests working

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe StatsController do
+ #render_views is NOT automatic. Need to put it here!
+  render_views
 
+  #green
   describe "GET stats" do
     it "should be successful" do
       get 'index'
       response.should be_success
+    end
+  end
+
+  #green
+  describe "GET stats page" do
+    it "should show stats message" do
+      get :index
+      response.should have_selector("h1", :content => "Rock, Paper, Scissors - Stats")
     end
   end
 
